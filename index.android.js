af2f98d9c1ce9f415da3461b2ffc040d6a256983
huangjilaiqin <1577594730@qq.com>
2016-12-07 20:53:02 +0800

    update

--- index.android.js
+++ index.android.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+import moment from 'moment';
 //import SocketIO from 'react-native-socketio';
 let SocketIO = require('react-native-socketio');
 
@@ -23,12 +24,12 @@ class NewsItem extends Component{
     super(props);
   }
   render(){
-    const news=this.props.news;
+    const recom=this.props.recom;
     return (
       <View 
-        key={news.key}
-        style={styles.newsItem}>
-        <Text>{news.title}</Text>
+        key={recom.key}
+        style={styles.recomItem}>
+        <Text>{recom.title}</Text>
       </View>
     );
   }
@@ -38,32 +39,51 @@ export default class demo extends Component {
   constructor(props){
     super(props);
     this.state={num:0};
-    this.maxId=2;
     this._randerRow=this._randerRow.bind(this);
     this._onEndReached=this._onEndReached.bind(this);
     this._renderSeparator=this._renderSeparator.bind(this);
-    this._pullNews=this._pullNews.bind(this);
+    //this._pullNews=this._pullNews.bind(this);
     this.itemPress=this.itemPress.bind(this);
     this.itemLongPress=this.itemLongPress.bind(this);
+    this.initData=this.initData.bind(this);
+    this.pullUp=this.pullUp.bind(this);
+    this.pullDonw=this.pullDonw.bind(this);
 
     this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.datas=[];
     this.dsData=this.ds.cloneWithRows(this.datas);
 
-    let newsType='yaowen';
-    this._pullNews(newsType,this.maxId);
-    this.maxId++;
+    //this._pullNews();
 
-    let socket = new SocketIO('http://106.75.146.152:5005',{'log':true});
-    socket.connect();
+    this.socket = new SocketIO('http://ws-live.qqshidao.com',{'log':true});
+    this.socket.connect();
     console.log('to connect');
-    socket.on('connect',()=>{console.log('connect')});
-    socket.on('error',(err)=>{console.log('err',err)});
-    socket.emit('myTest',{'pwd':'19910725'});
-    //socket.emit('myTest','client test');
-    socket.on('myTest',function(data){
-      console.log('data:',arguments);
+    this.socket.on('connect',()=>{console.log('connect')});
+    this.socket.on('error',(err)=>{console.log('err',err)});
+    this.socket.on('myTest',(args)=>{
+      console.log('data:',args);
+      let data=args[0];
+      let recoms=data['recoms'];
+      let updown=data['updown'];
+      let a=[1,3,5];
+      let b=[7,8,9];
+      let c=[...a,...b];
+      console.log(c);
+      
+      if(updown==1)
+        for(let r of recoms)
+          this.datas.unshift(r);
+      else
+        for(let r of recoms)
+          this.datas.push(r);
+
+      console.log(this.datas);
+      this.dsData=this.ds.cloneWithRows(this.datas);
+      this.setState((previousState, currentProps)=>{num:previousState.num++});
+      /*
+      */
     });
+    this.initData();
   }
 
   itemPress(){
@@ -72,65 +92,26 @@ export default class demo extends Component {
   itemLongPress(){
     console.log('itemLongPress');
   }
-  
 
-  _pullNews(newsType,page){
-    
-    let option={
-      method: 'GET',
-      headers:{
-        'Content-Type': 'application/json',
-        'User-Agent':'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36',
-        'Host':'c.m.163.com',
-        'Connection': 'Keep-Alive',
-        'Accept-Encoding': 'gzip',
-      }
-    };
-    //http://temp.163.com/special/00804KVA/cm_shehui_03.js?callback=data_callback
-    const url = 'http://temp.163.com/special/00804KVA/cm_'+newsType+'_0'+page+'.js?callback=data_callback';
-    let myresp=fetch(url)
-      .then((resp)=>{
-        return resp.text();
-      })
-      .then((respText)=>{
-        respText.replace('data_callback(','');
-        respText=respText.replace(/\)$/,'');
-        //console.log(respText);
-        respText='[{"title":"hj"},{"title":"hj1"},{"title":"hj2"},{"title":"hj3"},{"title":"hj4"},{"title":"hj5"}]';
-        respJson=JSON.parse(respText);
-        //console.log('respJson:',respJson);
-        return respJson;
-      })
-      .catch((error,r)=>{
-        console.log(error,r);
-        console.log(arguments);
-        this.maxId--;
-      });
-    myresp.then((respJson)=>{
-      //console.log(this.datas);
-
-      if(respJson){
-        respJson.map((data)=>{
-          item={
-            title:data.title,
-            imgsrc:data.imgurl,
-            source:'',
-            replyCount:data.tienum,
-          };
-          this.datas.push(item);
-        });
-        //console.log(this.datas);
-        this.dsData=this.ds.cloneWithRows(this.datas);
-        this.setState((previousState, currentProps)=>{num:previousState.num++});
-      }
-      })
-      .catch((error)=>{
-        console.log('handle back error:',error);
-        this.maxId--;
-      });
-    //console.log('myresp:',myresp);
-  
+  initData(){
+    this.socket.emit('myTest',{id:0,updown:-1});
   }
+
+  pullDonw(){
+    let id=0;
+    if(datas.length>0)
+      id=this.datas[0]['id'];
+    this.socket.emit('myTest',{id,updown:1});
+  }
+  
+  pullUp(){
+    let id=0;
+    if(datas.length>0)
+      id=this.datas[this.datas.length]['id'];
+    this.socket.emit('myTest',{id,updown:-1});
+  }
+
+    
 
   /*
   function getMoviesFromApiAsync() {
@@ -158,7 +139,7 @@ export default class demo extends Component {
   onPressEvent(e){
     console.log('onPressEvent:',e);
   }
-  _randerRow(news){
+  _randerRow(recom){
     return (
       <TouchableHighlight 
         underlayColor="red"
@@ -171,11 +152,13 @@ export default class demo extends Component {
         delayPressOut={5}
         >
         <View 
-          key={news.key}
-          style={styles.newsItem}>
-          <Image 
-            source={{uri:news.imgsrc}} 
-            style={{width:64,marginRight:8}}/>
+          key={recom.id}
+          style={styles.recomItem}>
+          <View>
+            <Text>{recom.simpleleague}</Text>
+            <Text>{recom.vsdate}</Text>
+            <Text>{recom.homesxname} vs {recom.awaysxname}</Text>
+          </View>
           <View 
             style={{flex:1,justifyContent:'space-between'}}>
             <Text 
@@ -184,7 +167,7 @@ export default class demo extends Component {
               style={{
                 fontSize:16,
                 color:'black',
-              }}>{news.title}</Text>
+              }}>{recom.title}</Text>
             <View 
               style={{
                 flex:1,
@@ -197,13 +180,13 @@ export default class demo extends Component {
                   fontSize:14,
                   color:'grey',
                 }}
-                >{news.source}</Text>
+                >{recom.source}</Text>
               <Text
                 style={{
                   fontSize:14,
                   color:'grey',
                 }}
-                >{news.replyCount}热度</Text>
+                >{recom.replyCount}热度</Text>
             </View>
           </View>
         </View>
@@ -212,8 +195,8 @@ export default class demo extends Component {
   }
   _onEndReached(){
     console.log('_onEndReached:',arguments);
-    let newsType='yaowen';
-    this._pullNews(newsType,this.maxId);
+    let recomType='yaowen';
+    //this._pullNews(recomType,this.maxId);
     this.maxId++;
   }
   _renderSeparator(sectionID: number, rowID: number, adjacentRowHighlighted: bool){
@@ -264,16 +247,16 @@ export default class demo extends Component {
   }
 }
 
+  //flexDirection:'row',
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   listStyle: {
   },
-  newsItem: {
+  recomItem: {
     flex:1,
-    flexDirection:'row',
-    height:80,
+    height:200,
     padding:8,
   },
   itemSeprator: {
